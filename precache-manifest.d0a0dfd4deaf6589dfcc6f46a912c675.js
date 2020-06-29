fbb1864c9e652f658dc29f4f964e8718e46d385f
yury_mac <polyantsevyury@gmail.com>
2020-06-29 15:07:14 +0400

    Updates

---
+++ precache-manifest.d0a0dfd4deaf6589dfcc6f46a912c675.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14d15f8089cad0397be9658070fa135",
+    "url": "/interstellar-weather-app/index.html"
+  },
+  {
+    "revision": "28e90cc2a7d0ea2d67cf",
+    "url": "/interstellar-weather-app/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "28e90cc2a7d0ea2d67cf",
+    "url": "/interstellar-weather-app/static/js/2.635d8d75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/interstellar-weather-app/static/js/2.635d8d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f04967dc897c4397f17",
+    "url": "/interstellar-weather-app/static/js/main.c3e995d8.chunk.js"
+  },
+  {
+    "revision": "d6ecb7b3a0c835c511a5",
+    "url": "/interstellar-weather-app/static/js/runtime-main.15f8552a.js"
+  },
+  {
+    "revision": "ad456a3a963c108547228ed3f3ed79be",
+    "url": "/interstellar-weather-app/static/media/context_camera.ad456a3a.PNG"
+  },
+  {
+    "revision": "464a0c640823de5b48dc78dd9c6b9eda",
+    "url": "/interstellar-weather-app/static/media/earth.464a0c64.jpg"
+  },
+  {
+    "revision": "0d4cbc41b163d58f62e238fe7cc94b49",
+    "url": "/interstellar-weather-app/static/media/mars.0d4cbc41.jpg"
+  }
+]);
